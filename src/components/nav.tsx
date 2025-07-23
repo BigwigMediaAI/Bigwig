3cd145620b3b42cbbfa1b2a49d0ba8a8391f44ae
shubham9049 <shubham9049>
2025-07-23 16:35:11 +0530

    Add changes

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -80,7 +80,12 @@ const Navbar = () => {
     <nav className="w-full bg-white border-b border-gray-200 sticky top-0 z-50">
       <div className="w-full max-w-7xl mx-auto flex items-center justify-between px-4 py-4">
         <a href="/" className="flex items-center space-x-1 text-3xl font-bold">
-          <img src={logo} alt="Bigwig Logo" className="h-10 w-auto" />
+          <img
+            src={logo}
+            alt="Bigwig Logo"
+            className="h-10 w-auto"
+            draggable="false"
+          />
         </a>
 
         {/* Desktop Nav */}
